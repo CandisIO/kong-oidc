259e4711a27859415ca46885564aa1cf9d618f0f
Fabian Schaffert <fabian@schaffert.cc>
2019-09-04 17:25:35 +0200

    CAN-1756: Add session_cookie_domain to schema

--- kong/plugins/oidc/schema.lua
+++ kong/plugins/oidc/schema.lua
@@ -15,6 +15,7 @@ return {
     ssl_verify = { type = "string", required = true, default = "no" },
     token_endpoint_auth_method = { type = "string", required = true, default = "client_secret_post" },
     session_secret = { type = "string", required = false },
+    session_cookie_domain = { type = "string", required = false },
     recovery_page_path = { type = "string" },
     logout_path = { type = "string", required = false, default = '/logout' },
     redirect_after_logout_uri = { type = "string", required = false, default = '/' },
